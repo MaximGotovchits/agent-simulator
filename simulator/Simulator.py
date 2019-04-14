10c16776f30f5cd766928235cb5e64dcc2774f0f
MaximGotovchits <Gotovchits.m@mail.ru>
2019-04-15 01:44:17 +0300

    Add 2d visualization.

--- simulator/Simulator.py
+++ simulator/Simulator.py
@@ -1,34 +1,114 @@
-import csv
 import random
+import ast
+
+
+class WorldMap:
+    objects = []
+    pos_to_last_update_time = {}
+    pos_to_state = {}
+    agents = []
+    current_time = 0
+    map_damage_factor = 0.8
+    map_damage_threshold = 0.6
+
+    def __init__(self, agents):
+        self.agents = agents
+
+    # def calculate_state(self, time):
+    #     return 1. - pow(self.map_damage_factor, self.current_time - time)
+    #
+    # def get_state(self, pos):
+    #     last_upd_time = 0
+    #     if self.pos_to_last_update_time.has_key(str(pos)):
+    #         last_upd_time = self.pos_to_last_update_time[str(pos)]
+    #     state = self.calculate_state(last_upd_time)
+    #     return state
+
+    def start_emulation(self):
+        whole_time = 20
+        total_result = []
+
+        for t in range(whole_time):
+            collisions = {}
+            positions_to_agents = {}
+            agent_num = 1
+
+            if t == 0:
+                self.init_world()
+            else:
+                self.update_world()
+            current_record = {}
+
+            for curr_pos in self.pos_to_state:
+                current_record[curr_pos] = str(self.pos_to_state[curr_pos])
+            total_result.append(current_record)
+
+            for current_agent in self.agents:
+                self.update_pos(str(current_agent.get_pos()))
+                current_pos = current_agent.do_step()
+                print "agent_" + str(agent_num) + " at pos: " + str(current_pos) + " at time: " + str(t)
+                if positions_to_agents.has_key(str(current_pos)):
+                    if len(positions_to_agents[str(current_pos)]) > 1:
+                        collisions[current_pos] = positions_to_agents[str(current_pos)]
+                        print "Collision at " + str(current_pos) + " between " + \
+                              str(len(positions_to_agents[str(current_pos)])) + " agents"
+                agent_num += 1
+            self.current_time += 1
+        with open('data/result/resultUpd', 'a') as result_file: # result_n | n - time
+            result_file.write(str(total_result))
+
+        # print str(self.pos_to_state)
+
+    def init_world(self):
+        for x in range(10):
+            for y in range(10):
+                self.pos_to_state[str([x, y])] = 0.5
+
+    def update_world(self):
+        for current_pos in self.pos_to_state.keys():
+            self.pos_to_state[current_pos] = round(self.pos_to_state[current_pos] * 0.9, 4)
+
+    def update_pos(self, pos):
+        self.pos_to_state[pos] = round(1. - self.pos_to_state[pos] * 0.9, 4)
+
 
 class Agent:
+    step_complete = 0.
     light_tp = .5
     light_fp = .5
 
-    sign_tp = .5
-    sign_fp = .5
+    sign_tp_prob = .5
+    sign_fp_prob = .5
 
-    path = None  # [0, 1, 2, 3, 4, 5, ..., N]
+    route = None
 
-    route = None  # [0, 1, 2, 1, 0, ..., N, ..., N]
+    current_step = 0
 
-    objects = None
+    current_pos = None
 
-    def __init__(self):
-        pass
+    def __init__(self, path_to_file):
+        self.route = {'speed': [], 'position': []}
+        with open(path_to_file, 'r') as agent_data:
+            for line in agent_data:
+                line_list = line.split(" ")
+                if len(line_list) == 3:
+                    self.route['position'].append([int(line_list[0]), int(line_list[1])])
+                    self.route['speed'].append(float(line_list[2]))
+        self.current_pos = self.route['position'][0]
+
+    def get_pos(self):
+        return self.current_pos
+
+    def get_observation(self, t):
+        1 - pow(1 - self.light_fp, t)
+        1 - pow(1 - self.sign_fp_prob, t)
 
     def set_model(self, light, sign):
         self.light_tp = light
         self.light_fp = 1. - light
 
-        self.sign_tp = sign
-        self.sign_fp = 1. - sign
-
-    def set_path(self, input_path):
-        self.path = input_path
-
-    def get_path(self):
-        return self.path
+        self.sign_tp_prob = sign
+        self.sign_fp_prob = 1. - sign
 
     def set_route(self, input_route):
         self.route = input_route
@@ -36,12 +116,9 @@ class Agent:
     def get_route(self):
         return self.route
 
-    def set_objects(self, objects):
-        self.objects = objects
-
     def get_probability(self, object_name):
         if "Sign" in object_name:
-            if random.randint(0, 10)/10. < self.sign_tp:
+            if random.randint(0, 10)/10. < self.sign_tp_prob:
                 return 1.
             else:
                 return 0.
@@ -52,61 +129,24 @@ class Agent:
                 return 0.
         pass
 
-    def run(self):
-        if self.path is None or self.route is None or self.objects is None:
-            return None
-        result = {}
-        for current_step in self.route:
-            current_step = int(current_step)
-            if self.objects[current_step] in result:
-                result[self.objects[current_step]] += self.get_probability(self.objects[current_step])
-            else:
-                result[self.objects[current_step]] = self.get_probability(self.objects[current_step])
-        # for object_name in result.keys():
-        #     result[object_name] = result[object_name] /
-        return result
+    def do_step(self):
+        self.current_step %= len(self.route['position'])
+        self.step_complete += self.route['speed'][self.current_step]
+        if self.step_complete >= 1:
+            self.step_complete = 0.
+            self.current_step += 1
+        self.current_step %= len(self.route['position'])
+        self.current_pos = self.route['position'][self.current_step]
+        return self.route['position'][self.current_step]
 
 
-def get_objects_and_probabilities(data):
-    pass
-
-
-def get_data_from_route_file(path_to_route):
-    output_path = range(26)
-    return output_path
-
-
-def get_objects_from_file(path_to_file):
-    with open(path_to_file, 'r') as csvfile:
-        data_reader = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
-        result = {i[0]: map(str, i[1:]) for i in zip(*data_reader) if i[0] in ('Number', 'Name')}
-    return result
-
-
-def run_simulation(path_to_route):
-    objects_and_order = get_objects_from_file(path_to_route)
-
-    # input_data = get_data_from_route_file(path_to_route)
-    forward_agent = Agent()
-    forward_agent.set_path(objects_and_order['Number'])
-    forward_agent.set_route(objects_and_order['Number'])
-    forward_agent.set_objects(objects_and_order['Name'])
-
-    backward_agent = Agent()
-    backward_agent.set_path(objects_and_order['Number'])
-    backward_agent.set_route(objects_and_order['Number'][::-1])
-    backward_agent.set_objects(objects_and_order['Name'])
-
-    first_result = forward_agent.run()
-    second_result = backward_agent.run()
-
-    result = {}
-
-    for object_name in first_result.keys():
-        result[object_name] = (first_result[object_name] + second_result[object_name]) / 2.
-    # print get_objects_and_probabilities(input_data)
-    return result
-
+def get_world_objects(path_to_world_objects):
+    objects = get_objects_from_file(path_to_world_objects)
+    return list(set(objects['Name'])) # Expecting that names are unique.
 
 if __name__ == "__main__":
-    print run_simulation("data/Route.csv")
+    agent1 = Agent("data/agent1")
+    agent2 = Agent("data/agent2")
+
+    world = WorldMap([agent1, agent2])
+    world.start_emulation()
